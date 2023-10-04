3df451392f7c20c6f555cc460c5fe951e0f87746
Daisy Chang <146611553+daisychangm@users.noreply.github.com>
2023-10-04 18:24:03 -0400

    implemented hover functionality on start button

--- my-react-project/src/pages/index.js
+++ my-react-project/src/pages/index.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // Images
 import logo from '../images/logo.png';
 import start from '../images/start-default.png';
- 
+import hoverStart from '../images/start-hover.png' 
+
 const StartPage = () => {
+    const [buttonImage, setButtonImage] = useState(start);
+
+    const handleHover = () => {
+        setButtonImage(hoverStart);
+    }
+
+    const handleHoverOut = () => {
+        setButtonImage(start);
+    }
+
     return (
         <div className='flex items-center flex-col'>
             <h1 className=''>welcome to</h1>
-            <img src={logo} alt="Logo image" />
+            <img src={logo} />
 
             <div className='w-20'>
                 <Link to="/Home">
                     <button>
-                        <img src={start} alt="Start image" />
+                        <img 
+                        src={buttonImage}
+                        onMouseOver={handleHover}
+                        onMouseOut={handleHoverOut}
+                    />
                     </button>
                 </Link>
                 
