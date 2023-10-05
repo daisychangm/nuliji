e261b88b3bdb4169f37abb0fba3170e4cac17b15
Daisy Chang <146611553+daisychangm@users.noreply.github.com>
2023-10-05 01:14:24 -0400

    Added link formatting

--- my-react-project/src/pages/SignUp.js
+++ my-react-project/src/pages/SignUp.js
@@ -8,7 +8,6 @@ const SignUp = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
     }
 
     return (
@@ -16,37 +15,39 @@ const SignUp = (props) => {
             <div className='text-center mt-[-30px]'>
                 <h1 style={{ fontSize: '30px' }}>Sign Up</h1>
 
-                <form onSubmit={handleSubmit}>
-                    <input 
-                        value={username} onChange={(e) => setUsername(e.target.value)}
-                        type='username'
-                        placeholder='username'
-                        id='username'
-                        name='username'
-                    />
-                    <br />
-                    <input 
-                        value={email} onChange={(e) => setEmail(e.target.value)}
-                        type='email'
-                        placeholder='email'
-                        id='email'
-                        name='email'
-                    />
-                    <br />
-                    <input 
-                        value={pass} onChange={(e) => setPass(e.target.value)}
-                        type='password'
-                        placeholder='password'
-                        id='password'
-                        name='password'
-                    />
-                    <br />
-                    <Link to="/Home">
-                        <button type='submit' style={{ marginTop: '20px', paddingLeft: '10px', paddingRight: '10px', border: '5px solid black', borderRadius: '10px' }}>Enter</button>
-                    </Link>
-                </form>
+                <div className='auth-form-container'>
+                    <form onSubmit={handleSubmit}>
+                        <input 
+                            value={username} onChange={(e) => setUsername(e.target.value)}
+                            type='username'
+                            placeholder='username'
+                            id='username'
+                            name='username'
+                        />
+                        <br />
+                        <input 
+                            value={email} onChange={(e) => setEmail(e.target.value)}
+                            type='email'
+                            placeholder='email'
+                            id='email'
+                            name='email'
+                        />
+                        <br />
+                        <input 
+                            value={pass} onChange={(e) => setPass(e.target.value)}
+                            type='password'
+                            placeholder='password'
+                            id='password'
+                            name='password'
+                        />
+                        <br />
+                        <Link to="/Home">
+                            <button type='submit' style={{ marginTop: '20px', paddingLeft: '10px', paddingRight: '10px', border: '5px solid black', borderRadius: '10px' }}>Enter</button>
+                        </Link>
+                    </form>
+                </div>
                 <br />
-                Already have an account? <button onClick={() => props.onFormSwitch('login')}>Log in here</button>
+                Already have an account? <button className='btn-link' onClick={() => props.onFormSwitch('login')}>Log in here</button>
             </div>
         </div>
     );
