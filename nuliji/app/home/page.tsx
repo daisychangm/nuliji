1d40f30dd6ec68d8dc210ac51025d5f395663e0d
Daisy Chang <daisychang.m@gmail.com>
2024-01-03 00:00:00 -0600

    Added font

---
+++ nuliji/app/home/page.tsx
@@ -0,0 +1,7 @@
+import { viga } from "../ui/fonts"
+
+export default function HomePage() {
+    return <div>
+        <h1 className={`${viga.className}`}>Home</h1>
+    </div>
+}
---
+++ nuliji/app/ui/fonts.ts
@@ -0,0 +1,19 @@
+import localFont from 'next/font/local'
+
+export const objectiveFont = localFont({ 
+    src: [
+        {
+            path: "fonts/Objective.ttf",
+        }
+    ],
+    variable: "--font-objective",
+})
+
+export const viga = localFont({ 
+    src: [
+        {
+            path: "fonts/Viga-Regular.ttf",
+        }
+    ],
+    variable: "--font-viga",
+})
